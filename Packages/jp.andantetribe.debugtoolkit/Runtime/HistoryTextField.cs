3d76ac9fd6d6f1510cfc0018ab82431fc71b7ec3
asahinnnure <136082695+asahinnnure@users.noreply.github.com>
2025-07-24 17:46:03 +0900

    Revert "Revert "feat: KeyDownEventを使用してundoとredoを実装したHistoryTextFieldクラスを作成""
    This reverts commit e0a34e8a280c65ec9d23d7aadfd748b898c4e485.

---
+++ Packages/jp.andantetribe.debugtoolkit/Runtime/HistoryTextField.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+using UnityEngine;
+
+namespace DebugToolkit
+{
+    public class HistoryTextField : TextField
+    {
+        private readonly Stack<string> _undoStack = new Stack<string>();
+        private readonly Stack<string> _redoStack = new Stack<string>();
+
+        private bool _isUndoRedoOperation;
+
+        public HistoryTextField() : this(null) { }
+
+        private HistoryTextField(string label) : base(label)
+        {
+            this.RegisterValueChangedCallback(OnValueChanged);
+
+            RegisterCallback<KeyDownEvent>(OnKeyDown);
+        }
+
+        private void OnValueChanged(ChangeEvent<string> evt)
+        {
+            if (_isUndoRedoOperation)
+            {
+                return;
+            }
+
+            _undoStack.Push(evt.previousValue);
+            _redoStack.Clear();
+        }
+
+        private void OnKeyDown(KeyDownEvent evt)
+        {
+            var isCtrlOrCmd = evt.ctrlKey || evt.commandKey;
+
+            if (isCtrlOrCmd)
+            {
+                if (evt.keyCode == KeyCode.Z)
+                {
+                    Undo();
+                    evt.StopPropagation();
+                }
+
+                if (evt.keyCode == KeyCode.Y)
+                {
+                    Redo();
+                    evt.StopPropagation();
+                }
+            }
+        }
+
+        private void Undo()
+        {
+            if (_undoStack.Count > 0)
+            {
+                _redoStack.Push(value);
+
+                _isUndoRedoOperation = true;
+                this.SetValueWithoutNotify(_undoStack.Pop());
+                _isUndoRedoOperation = false;
+            }
+        }
+
+        private void Redo()
+        {
+            if (_redoStack.Count > 0)
+            {
+                _undoStack.Push(value);
+
+                _isUndoRedoOperation = true;
+                this.SetValueWithoutNotify(_redoStack.Pop());
+                _isUndoRedoOperation = false;
+            }
+        }
+    }
+}
